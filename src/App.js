41639e742e7f401a7508454b3226b6db611ec8dc
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2021-03-29 02:45:01 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <h1>Welcome to React portfolio website</h1>
+      <h1>Welcome to React portfolio Project</h1>
       <h2>- shaif Arfan</h2>
     </div>
   );
