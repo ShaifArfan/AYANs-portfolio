417bfd628158e9e72e49a9fa4533d8f071945257
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2021-04-01 07:49:14 +0600

    update starter sites

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -11,6 +11,8 @@ const GlobalStyles = createGlobalStyle`
     --gray-1: #BCB4B4;
     --deep-dark: #1E1E1E;
     --gray-2: #363636;
+    --white : white;
+    --black: black;
   }
   html{
     font-size: 10px;
@@ -25,6 +27,8 @@ const GlobalStyles = createGlobalStyle`
   }
   img, svg{
     width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
   button{
     outline: none
--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,14 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 import RobotoMonoMedium from '../assets/fonts/RobotoMono-Medium.ttf';
 import RobotoMonoSemiBold from '../assets/fonts/RobotoMono-SemiBold.ttf';
-import RobotoMonoLight from '../assets/fonts/RobotoMono-Light.ttf';
+import RobotoMonoRegular from '../assets/fonts/RobotoMono-Regular.ttf';
 import MontserratMedium from '../assets/fonts/Montserrat-Medium.ttf';
 import MontserratSemiBold from '../assets/fonts/Montserrat-SemiBold.ttf';
 
 const Typography = createGlobalStyle`
-  @font-face {
-    font-family: 'RobotoMono Light';
-    src: url(${RobotoMonoLight});
+    @font-face {
+    font-family: 'RobotoMono Regular';
+    src: url(${RobotoMonoRegular});
     font-style: normal;
   }
   @font-face {
@@ -32,11 +32,11 @@ const Typography = createGlobalStyle`
     font-style: normal;
   }
   html{
-    font-family: 'RobotoMono Medium';
+    font-family: 'RobotoMono Regular';
     color: var(--gray-1);
   }
   *{
-    font-family: 'RobotoMono Medium';
+    font-family: 'RobotoMono Regular';
     color: var(--gray-1);
   }
   h1,h2,h3,h4,h5,h6{
